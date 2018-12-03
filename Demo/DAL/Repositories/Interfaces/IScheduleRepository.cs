be13fa5d2c8f474d8f7715d1c723798269c31017
minhtuan520 <minhtuan520@hotmail.com.vn>
2018-12-04 00:28:00 +0700

    delete client-app

---
+++ Demo/DAL/Repositories/Interfaces/IScheduleRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IScheduleRepository : IRepository<Tkb>
+    {
+        List<Tkb> GetAll();
+        List<Tkb> GetClassSchedule(string Hieuluc, int Id, string L);
+        List<Tkb> GetTeacherSchedule(string Hieuluc, int Id, string Gv);
+        List<Tkb> GetSchedule(string Hieuluc, int Id);        
+    }
+}
---
+++ Demo/DAL/Repositories/ScheduleRepository.cs
@@ -0,0 +1,50 @@
+﻿using DAL.Models;
+using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DAL.Repositories
+{
+    public class ScheduleRepository : Repository<Tkb>, IScheduleRepository
+    {
+        #region Constructer
+
+        public ScheduleRepository(tkbremake4DbContext context) : base(context)
+        {
+        }
+        private tkbremake4DbContext _appContext
+        {
+            get
+            {
+                return (tkbremake4DbContext)_context;
+            }
+        }
+
+        #endregion
+        #region Function
+        public List<Tkb> GetClassSchedule(string Hieuluc, int Id, string L)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tkb> GetSchedule(string Hieuluc, int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tkb> GetTeacherSchedule(string Hieuluc, int Id, string Gv)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tkb> GetAll()
+        {
+
+
+
+            throw new NotImplementedException();
+        } 
+        #endregion
+    }
+}
